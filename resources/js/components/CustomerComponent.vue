0fa2829d13272eed2cb9eb8ffb2ff801898adb87
soehl <sohelcse1999@gmail.com>
2019-08-08 15:01:28 +0600

    initial project

---
+++ resources/js/components/CustomerComponent.vue
@@ -0,0 +1,60 @@
+<template>
+    <div id="customer">
+        <div class="container">
+            <div class="row">
+                {{message}}
+                <div class="col-xl-12 col-lg-12">
+                    <div class="table-responsive">
+                        <table class="table table-bordered">
+                            <thead>
+                            <tr>
+                                <th scope="col">serial no</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Handle</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Larry</td>
+                                <td>the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+
+    export default {
+        data() {
+            return {
+                message: 'Hello World'
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/views/Frontend/customer.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="" id="app">
+        <customer></customer>
+    </div>
+@stop
